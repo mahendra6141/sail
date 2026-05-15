30bee2e672f59a70c08d9589a6f8ea2a26e20ee1
Mahendrasirvi <mahendrasirvi5000@gmail.com>
2026-05-15 12:31:05 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,12 @@
-const supabaseUrl = "https://bejhhnjpqqtnoninfphh.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlamhobmpwcXF0bm9uaW5mcGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTM1ODksImV4cCI6MjA5NDI4OTU4OX0.NOn00gvP1sLMsjIVECb4hnzLNjPZDeR3iNxfpTKMGi4";
 
-const supabase = window.supabase.createClient(
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl = "https://bejhhnjpqqtnoninfphh.supabase.co";
+
+const supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlamhobmpwcXF0bm9uaW5mcGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTM1ODksImV4cCI6MjA5NDI4OTU4OX0.NOn00gvP1sLMsjIVECb4hnzLNjPZDeR3iNxfpTKMGi4";
+
+const supabase = createClient(
   supabaseUrl,
   supabaseKey
 );
